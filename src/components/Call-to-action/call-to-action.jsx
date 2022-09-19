86cb81392fbdcbc4d71bff9d85a563227814df8b
Tashila <tashila.dev@gmail.com>
2022-09-19 23:46:00 +0400

    Initialized call to action section

--- src/components/Call-to-action/call-to-action.jsx
+++ src/components/Call-to-action/call-to-action.jsx
@@ -1,45 +1,39 @@
-import React from 'react'
-import Split from '../Split'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+import classNames from 'classnames';
 
-const CallToAction = ({img}) => {
-    return (
-      <section
-        className="call-action section-padding sub-bg bg-img"
-        style={{ backgroundImage: `url(${img ? img : "/img/patrn.svg"})` }}
-      >
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 col-lg-9">
-              <div className="content sm-mb30">
-                <Split>
-                  <h6 className="wow words chars splitting" data-splitting>
-                    Let’s Talk
-                  </h6>
-                  <h2 className="wow words chars splitting" data-splitting>
-                    about your <br /> <b className="back-color">next project</b>
-                    .
-                  </h2>
-                </Split>
-              </div>
-            </div>
+import Split from '../Split';
+import styles from './call-to-action.module.css';
 
-            <div className="col-md-4 col-lg-3 valign">
-              <Link
-                href={`/contact/contact-dark`}
-              >
-                <a
-                  className="butn bord curve wow fadeInUp"
-                  data-wow-delay=".5s"
-                >
-                  <span>Get In Touch</span>
-                </a>
-              </Link>
+const CallToAction = ({ img }) => {
+  return (
+    <section className='call-action section-padding sub-bg bg-img' style={{ backgroundImage: `url(${img ? img : '/img/patrn.svg'})` }}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 col-lg-9'>
+            <div className='content sm-mb30'>
+              <Split>
+                <h6 className='wow words chars splitting' data-splitting>
+                  Let’s Talk
+                </h6>
+                <h2 className='wow words chars splitting' data-splitting>
+                  about anything <br /> <b className='back-color'>on code and tech</b>.
+                </h2>
+              </Split>
             </div>
           </div>
-        </div>
-      </section>
-    );
-}
 
-export default CallToAction
+          <div className={classNames('col-md-4 col-lg-3 valign', styles.Buttons)}>
+            <Link href={`/contact/contact-dark`}>
+              <a className='butn bord curve wow fadeInUp' data-wow-delay='.5s'>
+                <span>Get In Touch</span>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CallToAction;
