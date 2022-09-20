b673e133026b960f5e6a1e8aff8a0d5e35f54a42
Tashila <tashila.dev@gmail.com>
2022-09-20 10:02:30 +0400

    Removed column in footer component

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import Link from 'next/link';
-import appData from '../../data/app.json';
 
 const Footer = ({ hideBGCOLOR }) => {
   return (
     <footer className={`${!hideBGCOLOR ? 'sub-bg' : ''}`}>
       <div className='container'>
         <div className='row'>
-          <div className='col-lg-4'>
+          <div className='col-lg-6'>
             <div className='item md-mb50'>
               <div className='title'>
                 <h5>Contact Me</h5>
@@ -18,7 +17,7 @@ const Footer = ({ hideBGCOLOR }) => {
                   <span className='icon pe-7s-mail'></span>
                   <div className='cont'>
                     <h6>Email Me</h6>
-                    <p>support@gmail.com</p>
+                    <p>tashila.dev@gmail.com</p>
                   </div>
                 </li>
               </ul>
@@ -46,7 +45,7 @@ const Footer = ({ hideBGCOLOR }) => {
             </div>
           </div>
 
-          <div className='col-lg-4'>
+          <div className='col-lg-6'>
             <div className='item md-mb50'>
               <div className='title'>
                 <h5>Recent Blog Posts</h5>
@@ -94,38 +93,13 @@ const Footer = ({ hideBGCOLOR }) => {
                     </Link>
                   </div>
                 </li>
-                <li>
+                {/* <li>
                   <div className='subscribe'>
                     <input type='text' placeholder='Type Your Email' />
                     <span className='subs pe-7s-paper-plane'></span>
                   </div>
-                </li>
+                </li> */}
               </ul>
-            </div>
-          </div>
-
-          <div className='col-lg-4'>
-            <div className='item'>
-              <div className='logo color-font'>Tashila Fernando</div>
-              <div className='social'>
-                <a href='#0'>
-                  <i className='fab fa-facebook-f'></i>
-                </a>
-                <a href='#0'>
-                  <i className='fab fa-twitter'></i>
-                </a>
-                <a href='#0'>
-                  <i className='fab fa-instagram'></i>
-                </a>
-                <a href='#0'>
-                  <i className='fab fa-youtube'></i>
-                </a>
-              </div>
-              <div className='copy-right'>
-                <p>
-                  © Tashila Fernando {new Date().getFullYear()}. <br /> Made with NextJS and passion
-                </p>
-              </div>
             </div>
           </div>
         </div>
