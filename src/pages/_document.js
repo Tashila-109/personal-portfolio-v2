07b0f7277bf55dc65fe2614096566a1c7d347173
Tashila <tashila.dev@gmail.com>
2022-09-21 21:59:07 +0400

    Fixed script document issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,32 +11,25 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="keywords"
-            content="Personal Portfolio"
-          />
-          <meta
-            name="description"
-            content="This is my personal portfolio"
-          />
-          <meta name="author" content="" />
-          <link rel="shortcut icon" href="/img/favicon.ico" />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          <meta name='keywords' content='Personal Portfolio' />
+          <meta name='description' content='This is my personal portfolio' />
+          <meta name='author' content='' />
+          <link rel='shortcut icon' href='/img/favicon.ico' />
+          <link href='https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap' rel='stylesheet' />
           <link
-            href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap'
+            rel='stylesheet'
           />
         </Head>
 
         <body>
           <Main />
           <NextScript />
+          <Script strategy='beforeInteractive' id='splitting' src='/js/splitting.min.js'></Script>
+          <Script id='isotope' strategy='beforeInteractive' src='/js/isotope.pkgd.min.js'></Script>
         </body>
       </Html>
     );
